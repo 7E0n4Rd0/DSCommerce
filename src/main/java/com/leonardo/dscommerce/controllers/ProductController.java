7f3cc78b31985029b387795b8b1041ed790804a5
leonArch <leomeireles66@gmail.com>
2024-12-23 12:37:07 -0300

    GET Product by id

---
+++ src/main/java/com/leonardo/dscommerce/controllers/ProductController.java
@@ -0,0 +1,27 @@
+package com.leonardo.dscommerce.controllers;
+
+import com.leonardo.dscommerce.DTO.ProductDTO;
+import com.leonardo.dscommerce.entities.Product;
+import com.leonardo.dscommerce.repositories.ProductRepository;
+import com.leonardo.dscommerce.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping(value = "/{id}")
+    public ProductDTO findById(@PathVariable Long id){
+        ProductDTO dto = service.findById(id);
+        return dto;
+    }
+}
---
+++ src/main/java/com/leonardo/dscommerce/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.leonardo.dscommerce.repositories;
+
+import com.leonardo.dscommerce.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
