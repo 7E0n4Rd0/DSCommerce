bd84516ac65dbc40ca7a8e13dd401e644360ec57
Leonardo <leomeireles66@gmail.com>
2025-03-19 12:44:59 -0300

    Search Product by Name test

--- src/test/java/com/leonardo/dscommerce/controllers/ProductControllerRA.java
+++ src/test/java/com/leonardo/dscommerce/controllers/ProductControllerRA.java
@@ -6,8 +6,11 @@ import static org.hamcrest.Matchers.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 public class ProductControllerRA {
 
+    private String productName;
     private Long existingId, nonExistingId;
 
     @BeforeEach
@@ -15,7 +18,7 @@ public class ProductControllerRA {
         baseURI = "http://localhost:8080";
         existingId = 2L;
         nonExistingId = 50L;
-
+        productName = "PC Gamer X";
     }
 
     @Test
@@ -32,6 +35,35 @@ public class ProductControllerRA {
             .body("categories.name", hasItems("Eletrônicos", "Computadores"));
     }
 
+    @Test
+    public void findAllShouldReturnPageWhenParamIsEmpty(){
+        given()
+            .get("/products")
+        .then()
+            .statusCode(200)
+            .body("content.name", hasItems("Macbook Pro", "PC Gamer Tera"));
+    }
 
+    @Test
+    public void findAllShouldReturnPageWhenParamNotIsEmpty(){
+        given()
+                .get("/products?name={name}", productName)
+        .then()
+                .statusCode(200)
+                .body("content[0].id", is(7))
+                .body("content[0].name", equalTo("PC Gamer X"))
+                .body("content[0].price", is(1350.0F))
+                .body("content[0].imgUrl", equalTo("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/7-big.jpg"))
+                .body("pageable.pageNumber", is(0));
+    }
+
+    @Test
+    public void findAllShouldReturnPageWhenWithPriceMoreThanTwoThousand(){
+        given()
+            .get("/products?size=25")
+        .then()
+            .statusCode(200)
+            .body("content.findAll {it.price > 2000}.name", hasItems("Smart TV", "PC Gamer Weed"));
+    }
 
 }
